cefec5bcea17854efdbabe34518c2fdcd45f86aa
Pradum Kumar <140975987+pradumkumar6@users.noreply.github.com>
2025-10-06 18:56:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function firstChar(text) {
   // your code here
+	// Trim leading spaces and return the first character if exists
+  const trimmed = str.trimStart();
+  return trimmed.length > 0 ? trimmed[0] : '';
 }
 
 // Do not change the code below
