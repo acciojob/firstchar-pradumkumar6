cd50eb69da0b5288a311b738b3f4001e30802401
Pradum Kumar <140975987+pradumkumar6@users.noreply.github.com>
2025-10-07 09:22:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 function firstChar(text) {
-  // your code here
-	// Trim leading spaces and return the first character if exists
+  // Trim leading spaces and return the first character if exists
   const trimmed = text.trimStart();
   return trimmed.length > 0 ? trimmed[0] : '';
 }
 
 // Do not change the code below
-//Uncomment the following line to show the prompt popup
-//const text = prompt("Enter text:");
+// Uncomment the following line to show the prompt popup
+const text = prompt("Enter text:");
 alert(firstChar(text));
